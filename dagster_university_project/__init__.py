c59e963d6708bcc5e0ff945c2acba53e20515109
seanpden <seanden522@gmail.com>
2024-04-18 20:25:10 -0500

    added asset groups by file

--- dagster_university_project/__init__.py
+++ dagster_university_project/__init__.py
@@ -7,9 +7,9 @@ from .jobs import trip_update_job, weekly_update_job, adhoc_request_job
 from .sensors import adhoc_request_sensor
 from .schedules import trip_update_schedule, weekly_update_schedule
 
-trip_assets = load_assets_from_modules([trips])
-metric_assets = load_assets_from_modules([metrics])
-request_assets = load_assets_from_modules([request])
+trip_assets = load_assets_from_modules([trips], group_name="trips")
+metric_assets = load_assets_from_modules([metrics], group_name="metrics")
+request_assets = load_assets_from_modules([request], group_name="request")
 all_assets = [*trip_assets, *metric_assets, *request_assets]
 
 all_resources = {
